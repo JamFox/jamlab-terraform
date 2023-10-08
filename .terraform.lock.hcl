5f60e3bc8458585bfba4c5ebfd41324197a70242
JamFox <github@jamfox.dev>
2023-10-08 16:54:19 +0300

    add vb vs nodes

---
+++ .terraform.lock.hcl
@@ -0,0 +1,62 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.4.0"
+  hashes = [
+    "h1:R97FTYETo88sT2VHfMgkPU3lzCsZLunPftjSI5vfKe8=",
+    "zh:53604cd29cb92538668fe09565c739358dc53ca56f9f11312b9d7de81e48fab9",
+    "zh:66a46e9c508716a1c98efbf793092f03d50049fa4a83cd6b2251e9a06aca2acf",
+    "zh:70a6f6a852dd83768d0778ce9817d81d4b3f073fab8fa570bff92dcb0824f732",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:82a803f2f484c8b766e2e9c32343e9c89b91997b9f8d2697f9f3837f62926b35",
+    "zh:9708a4e40d6cc4b8afd1352e5186e6e1502f6ae599867c120967aebe9d90ed04",
+    "zh:973f65ce0d67c585f4ec250c1e634c9b22d9c4288b484ee2a871d7fa1e317406",
+    "zh:c8fa0f98f9316e4cfef082aa9b785ba16e36ff754d6aba8b456dab9500e671c6",
+    "zh:cfa5342a5f5188b20db246c73ac823918c189468e1382cb3c48a9c0c08fc5bf7",
+    "zh:e0e2b477c7e899c63b06b38cd8684a893d834d6d0b5e9b033cedc06dd7ffe9e2",
+    "zh:f62d7d05ea1ee566f732505200ab38d94315a4add27947a60afa29860822d3fc",
+    "zh:fa7ce69dde358e172bd719014ad637634bbdabc49363104f4fca759b4b73f2ce",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.2.1"
+  hashes = [
+    "h1:FbGfc+muBsC17Ohy5g806iuI1hQc4SIexpYCrQHQd8w=",
+    "zh:58ed64389620cc7b82f01332e27723856422820cfd302e304b5f6c3436fb9840",
+    "zh:62a5cc82c3b2ddef7ef3a6f2fedb7b9b3deff4ab7b414938b08e51d6e8be87cb",
+    "zh:63cff4de03af983175a7e37e52d4bd89d990be256b16b5c7f919aff5ad485aa5",
+    "zh:74cb22c6700e48486b7cabefa10b33b801dfcab56f1a6ac9b6624531f3d36ea3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:79e553aff77f1cfa9012a2218b8238dd672ea5e1b2924775ac9ac24d2a75c238",
+    "zh:a1e06ddda0b5ac48f7e7c7d59e1ab5a4073bbcf876c73c0299e4610ed53859dc",
+    "zh:c37a97090f1a82222925d45d84483b2aa702ef7ab66532af6cbcfb567818b970",
+    "zh:e4453fbebf90c53ca3323a92e7ca0f9961427d2f0ce0d2b65523cc04d5d999c2",
+    "zh:e80a746921946d8b6761e77305b752ad188da60688cfd2059322875d363be5f5",
+    "zh:fbdb892d9822ed0e4cb60f2fedbdbb556e4da0d88d3b942ae963ed6ff091e48f",
+    "zh:fca01a623d90d0cad0843102f9b8b9fe0d3ff8244593bd817f126582b52dd694",
+  ]
+}
+
+provider "registry.terraform.io/telmate/proxmox" {
+  version     = "2.9.11"
+  constraints = "2.9.11"
+  hashes = [
+    "h1:RKM2pvHNJrQKcMD7omaPiM099vWGgDnnZqn1kGknYXU=",
+    "zh:0db1e3940cf208e56919e68c6d557dfc87d380316a474c8999916308bf991440",
+    "zh:2a0ae7af5b2f96d53b24f34575bc72ccbb79cab870901f26f00a301613f7c69e",
+    "zh:2f9eb4a4d2c5db04ec0940d7e250aaf1bac559acc787a5883688ba42159f8b8e",
+    "zh:362a5b44995a51c8de78f0106aa7741f212bb15fbf2d7477794ea3ee63e2c17d",
+    "zh:4d212404b741848cef1e469e390ad1df659bbfa8d47cd079d82d83c288925438",
+    "zh:54a65a01946839db263f8da389791863f6909db9d5fcfdb472e23b14883a5b6c",
+    "zh:5dfc95303efc53686b23762dfa4c50d887eb4cc0a3e9d527adc29b3a9f0439eb",
+    "zh:68db84c007cbdd7267d1f7b767b0b2b91e9ee2e2b92ac1d8a1568f3bc61e67cd",
+    "zh:85d45466445883ae64eed3d5fcb996de389ecf9268f0f7d2f22911fb3f56a344",
+    "zh:8673f8c794ea8413dc9a3933902492b3e5be99e79bc611fcef415be7d7268210",
+    "zh:d5041f72f550f3c81dafecb4e7dfca9f849737154a0e2c81434df6c72d75af25",
+    "zh:e60e03b495dd76660784a8ab07d8db0ce1df7165e713efb350c1864d92f87a8c",
+    "zh:ed1f75a2fe7d764356119a590f301ab8fd40cfeea78a514450868beb92115f28",
+    "zh:efa4140b78775509665370c915e60c9043a1325d608f96da151f8f7fcc7cb45e",
+  ]
+}
--- main.tf
+++ main.tf
@@ -7,7 +7,207 @@ module "infra" {
   connection_private_key_path = var.connection_private_key_path
 
   vms = {
-  } # End: vms
+    # BEGIN: Virtual Nomad Base Nodes
+    "vb0" = {
+      ipconfig0 = "ip=192.168.10.120/16,gw=192.168.10.1"
+      hostname  = "vb0"
+      fqdn      = "vb0.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Base Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vb0
+    "vb1" = {
+      ipconfig0 = "ip=192.168.10.121/16,gw=192.168.10.1"
+      hostname  = "vb1"
+      fqdn      = "vb1.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Base Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vb1
+    "vb2" = {
+      ipconfig0 = "ip=192.168.10.122/16,gw=192.168.10.1"
+      hostname  = "vb2"
+      fqdn      = "vb2.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Base Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vb2
+    # BEGIN: Virtual Nomad Service Nodes
+    "vs0" = {
+      ipconfig0 = "ip=192.168.10.130/16,gw=192.168.10.1"
+      hostname  = "vs0"
+      fqdn      = "vs0.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Service Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vs0
+    "vs1" = {
+      ipconfig0 = "ip=192.168.10.131/16,gw=192.168.10.1"
+      hostname  = "vs1"
+      fqdn      = "vs1.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Service Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vs1
+    "vs2" = {
+      ipconfig0 = "ip=192.168.10.132/16,gw=192.168.10.1"
+      hostname  = "vs2"
+      fqdn      = "vs2.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Virtual Nomad Service Node"
+      memory  = 12288
+      sockets = 2
+      cores   = 2
+      disks = [
+        {
+          size     = "150G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: vs2
+  }    # End: vms
 }
 
 module "sandbox" {
