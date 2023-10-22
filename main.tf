d813c2fb2edaf2fc57d26d0d374349a7bdf1904f
JamFox <github@jamfox.dev>
2023-10-22 17:26:05 +0300

    add head node

--- main.tf
+++ main.tf
@@ -7,6 +7,40 @@ module "infra" {
   connection_private_key_path = var.connection_private_key_path
 
   vms = {
+    # BEGIN: Base nodes
+    "head" = {
+      ipconfig0 = "ip=192.168.10.10/16,gw=192.168.10.1"
+      hostname  = "head"
+      fqdn      = "head.jamfox.dev"
+      username  = "ubuntu"
+      ssh_authorized_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILayJ7ZP6Z6IDms4ujnz9nRnAJEIXwyto1SbSwfzjqst jamfox"
+      ]
+      sudo_config = "['ALL=(ALL) NOPASSWD:ALL']"
+      runcmd = [
+        "sudo apt update && sudo apt upgrade -y",
+        "sudo /usr/sbin/reboot"
+      ]
+      # VMS
+      clone   = "ubuntu22"
+      desc    = "Head Node"
+      memory  = 1024
+      sockets = 2
+      cores   = 1
+      disks = [
+        {
+          size     = "50G"
+          iothread = 1
+          ssd      = 0
+        }
+      ]
+      networks = [
+        {
+          bridge   = "vmbr0"
+          firewall = false
+        }
+      ]
+    }, # End: head
     # BEGIN: Virtual Nomad Base Nodes
     "vb0" = {
       ipconfig0 = "ip=192.168.10.120/16,gw=192.168.10.1"
